1cb091d63d3a5332b4069fa4fdfc7a40be4bbf87
Brasnyo Krisztian <krisztianbrasnyo@gmail.com>
2022-02-09 12:59:38 +0100

    Javascript megirasa

---
+++ az.js
@@ -0,0 +1,5 @@
+let hamburger = document.getElementById("burger").addEventListener('click', dropdownaz);
+
+function dropdownaz(){
+    document.getElementById("dropdown").classList.toggle('active')        
+}
